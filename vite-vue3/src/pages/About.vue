9a36c0dfecf4425067c58abec0b1de1815c9a5c9
LizzieFeng <yiru0212@163.com>
2021-10-28 16:41:25 +0800

    vue3 vite 基础架子搭建

---
+++ vite-vue3/src/pages/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        关于
+    </h1>
+</template>
---
+++ vite-vue3/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>
+        我是首页，你好
+    </h1>
+</template>
+<script setup>
+if(true) {
+    console.log('dd')
+}
+</script>

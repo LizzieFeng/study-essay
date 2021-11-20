450512a86f8b32975d434bd02f82b23a1219ce99
LizzieFeng <yiru0212@163.com>
2021-11-20 15:05:46 +0800

    多页面打包

--- study-webpack/webpack.prod.js
+++ study-webpack/webpack.prod.js
@@ -4,12 +4,44 @@ const OptimizeCssAssetsPlugin  = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // webpack 5.61
 // const CleanWebpackPlugin = require('clean-webpack-plugin'); // webpack 4
+const glob = require('glob');
+
+const setMPA = () => {
+    const entry = {};
+    const htmlWebpackPlugins = [];
+    const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
+    Object.keys(entryFiles).map((index) => {
+        const entryFile = entryFiles[index];
+        // D:/githubProject/study-essay/study-webpack/src/index/index.js
+        const match = entryFile.match(/src\/(.*)\/index\.js/);
+        const pageName = match && match[1];
+        entry[pageName] = entryFile;
+        const hwpc = new HtmlWebpackPlugin({
+            template: path.join(__dirname, `src/${pageName}/index.html`), // htmlwebpackplugin的html模板的所在位置 模板里面可以使用js的语法
+            filename: `${pageName}.html`, // 指定打包出来的html的文件名。
+            chunks: [pageName], // 指定生成的html要使用哪些chunk， 和entry中的search
+            inject: true, // 打包出来的chunk会自动的注入到这个html中。
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCss: true,
+                minifyJs: true,
+                removeComments: false,
+            }
+        });
+        htmlWebpackPlugins.push(hwpc);
+    });
+    return {
+        entry,
+        htmlWebpackPlugins
+    };
+}
+const {entry, htmlWebpackPlugins} = setMPA();
+
 module.exports = {
     mode: 'production',
-    entry: {
-        index: './src/index/index.js',
-        search: './src/search/index.js'
-    },
+    entry: entry,
     output:{
         path: path.join(__dirname, 'dist'),
         filename: '[name]_[chunkhash:8].js', // 指纹
@@ -103,35 +135,35 @@ module.exports = {
                 // cssProcessor: require('cssnano'),
             }
         ),
-        // 通常而言，一个页面需要对应一个htmlwebpackplugin
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/search/index.html'), // htmlwebpackplugin的html模板的所在位置 模板里面可以使用js的语法
-            filename: 'search.html', // 指定打包出来的html的文件名。
-            chunks: ['search'], // 指定生成的html要使用哪些chunk， 和entry中的search
-            inject: true, // 打包出来的chunk会自动的注入到这个html中。
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCss: true,
-                minifyJs: true,
-                removeComments: false,
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/index/index.html'), // htmlwebpackplugin的html模板的所在位置 模板里面可以使用js的语法
-            filename: 'index.html', // 指定打包出来的html的文件名。
-            chunks: ['index'], // 指定生成的html要使用哪些chunk， 和entry中的search
-            inject: true, // 打包出来的chunk会自动的注入到这个html中。
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCss: true,
-                minifyJs: true,
-                removeComments: false,
-            }
-        }),
+        // // 通常而言，一个页面需要对应一个htmlwebpackplugin
+        // new HtmlWebpackPlugin({
+        //     template: path.join(__dirname, 'src/search/index.html'), // htmlwebpackplugin的html模板的所在位置 模板里面可以使用js的语法
+        //     filename: 'search.html', // 指定打包出来的html的文件名。
+        //     chunks: ['search'], // 指定生成的html要使用哪些chunk， 和entry中的search
+        //     inject: true, // 打包出来的chunk会自动的注入到这个html中。
+        //     minify: {
+        //         html5: true,
+        //         collapseWhitespace: true,
+        //         preserveLineBreaks: false,
+        //         minifyCss: true,
+        //         minifyJs: true,
+        //         removeComments: false,
+        //     }
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: path.join(__dirname, 'src/index/index.html'), // htmlwebpackplugin的html模板的所在位置 模板里面可以使用js的语法
+        //     filename: 'index.html', // 指定打包出来的html的文件名。
+        //     chunks: ['index'], // 指定生成的html要使用哪些chunk， 和entry中的search
+        //     inject: true, // 打包出来的chunk会自动的注入到这个html中。
+        //     minify: {
+        //         html5: true,
+        //         collapseWhitespace: true,
+        //         preserveLineBreaks: false,
+        //         minifyCss: true,
+        //         minifyJs: true,
+        //         removeComments: false,
+        //     }
+        // }),
         new CleanWebpackPlugin(),
-    ],
+    ].concat(htmlWebpackPlugins),
 };

9ea59746b3cced07a25fe2e606e147706884576d
LizzieFeng <yiru0212@163.com>
2021-11-20 10:33:37 +0800

    px自动转换rem

--- study-webpack/webpack.prod.js
+++ study-webpack/webpack.prod.js
@@ -45,6 +45,13 @@ module.exports = {
                             },
                         }
                     },
+                    {
+                        loader: 'px2rem-loader',
+                        options: {
+                            remUnit: 75, 
+                            remPrecision: 8
+                        }
+                    },
                     'less-loader',
                 ]
             },

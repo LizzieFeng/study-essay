687b5b76ae8fb101c7867fc9833d275a3bbd8ade
LizzieFeng <yiru0212@163.com>
2023-02-24 15:37:17 +0800

    feature: webpack第一个helloworddemo

---
+++ study-webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import _ from "lodash";
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    // element.innerHTML = "Hellow Webpack"
+
+    return element;
+}
+
+document.body.appendChild(component());

b524cfa5c2e86605f606d5527c2f3c81d276c6fe
LizzieFeng <yiru0212@163.com>
2023-03-01 10:05:40 +0800

    feature: module配置

--- study-webpack-demo/src/index.js
+++ study-webpack-demo/src/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
     // element.innerHTML = "Hellow Webpack"
+    element.classList.add('hello');
 
     return element;
 }
